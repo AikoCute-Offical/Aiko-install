64688202b01016e95d86ec8adc84b82aa558e839
AikoCute-Offical <aikocute@icloud.com>
2022-06-19 00:56:03 +0700

    Update aikocute.sh

--- aikocute.sh
+++ aikocute.sh
@@ -139,7 +139,7 @@ config_docker() {
 version: '3'
 services: 
   xrayr: 
-    image: aikocute/maxprovpn:1.3.12
+    image: aikocute/maxprovpn:v1.3.12
     volumes:
       - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
       - ./dns.json:/etc/XrayR/dns.json 
